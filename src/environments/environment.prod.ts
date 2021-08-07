522fa7930f2acd6583777121bccebffee86220d1
Sekab <ataher@inmobly.com>
2021-08-07 11:56:58 +0200

    feat(init): :wrench: added Open Weather API keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  OPEN_WEATHER_API: '06d741d301ded1e2207c00bece04f14e',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  OPEN_WEATHER_API: '06d741d301ded1e2207c00bece04f14e',
 };
 
 /*
